7678d0bda485d334bcf7e8b9559fdfcb276c1d8b
Wouter Rutgers <git@wouter2203.com>
2018-06-07 08:51:40 +0200

    compile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,14 @@
 const mix = require('laravel-mix');
 
+mix.setPublicPath('./');
+
 if (process.env.TESTING) {
   mix.js('./tests/test.js', './tests/compiled/');
 } else {
   mix.webpackConfig({
-      output: {
-        library: 'FuzzySearch',
-        libraryTarget: 'umd',
-      },
-    })
-    .js('src/FuzzySearch.js', 'dist/');
+    output: {
+      library: 'FuzzySearch',
+      libraryTarget: 'umd',
+    },
+  }).js('src/FuzzySearch.js', 'dist/');
 }
